7e28fd8927e6202cf5e415548c497a7280f04b18
Anthony Anjorin <tonyanjorin@googlemail.com>
2017-06-16 16:10:16 +0200

    Switch to using Filter NACs and PACs per Default

--- org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/compiler/pattern/rulepart/support/DEC/NoDECsPatterns.java
+++ org.emoflon.ibex.tgg.core.runtime/src/org/emoflon/ibex/tgg/compiler/pattern/rulepart/support/DEC/NoDECsPatterns.java
@@ -29,7 +29,7 @@ public class NoDECsPatterns extends RulePartPattern {
 	private DECTrackingHelper decTC;
 	private DomainType domain;
 	
-	private DECStrategy strategy = DECStrategy.NONE;
+	private DECStrategy strategy = DECStrategy.FILTER_NACS_AND_PACS;
 
 	public NoDECsPatterns(TGGRule rule, DECTrackingHelper decTC, DomainType domain) {
 		super(rule);
