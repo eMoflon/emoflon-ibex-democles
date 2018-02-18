7b96a8b2a93261c0cab78e807662aa4c7a7161ec
Patrick Robrecht <mail@patrick-robrecht.de>
2018-02-18 20:30:58 +0100

    Implement interface instead of inheritance

--- org.emoflon.ibex.gt.democles/src/org/emoflon/ibex/gt/democles/runtime/DemoclesGTEngine.java
+++ org.emoflon.ibex.gt.democles/src/org/emoflon/ibex/gt/democles/runtime/DemoclesGTEngine.java
@@ -2,10 +2,11 @@ package org.emoflon.ibex.gt.democles.runtime;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.emoflon.ibex.common.operational.IPatternInterpreter;
 import org.emoflon.ibex.common.utils.ModelPersistenceUtils;
-import org.emoflon.ibex.gt.engine.GTEngine;
 import org.gervarro.democles.event.MatchEvent;
 import org.gervarro.democles.event.MatchEventListener;
 import org.gervarro.democles.specification.emf.Pattern;
@@ -13,24 +14,34 @@ import org.gervarro.democles.specification.emf.Pattern;
 import IBeXLanguage.IBeXPatternSet;
 
 /**
- * Engine for unidirectional graph transformations with Democles.
- * 
- * @author Patrick Robrecht
- * @version 0.1
+ * Engine for graph transformations with Democles.
  */
-public class DemoclesGTEngine extends GTEngine implements MatchEventListener {
+public class DemoclesGTEngine implements IPatternInterpreter, MatchEventListener {
+	/**
+	 * The path for debugging output.
+	 */
+	protected Optional<String> debugPath = Optional.empty();
+
 	/**
 	 * The Democles patterns.
 	 */
 	private List<Pattern> patterns = new ArrayList<Pattern>();
 
 	@Override
-	protected void transformPatterns(final IBeXPatternSet ibexPatternSet) {
+	public void setDebugPath(final String debugPath) {
+		this.debugPath = Optional.of(debugPath);
+	}
+
+	@Override
+	public void initPatterns(final IBeXPatternSet ibexPatternSet) {
 		IBeXToDemoclesPatternTransformation transformation = new IBeXToDemoclesPatternTransformation();
 		this.patterns = transformation.transform(ibexPatternSet);
 		this.savePatternsForDebugging();
 	}
 
+	/**
+	 * Saves the Democles patterns for debugging.
+	 */
 	private void savePatternsForDebugging() {
 		this.debugPath.ifPresent(path -> {
 			List<Pattern> sortedPatterns = this.patterns.stream()
@@ -41,7 +52,7 @@ public class DemoclesGTEngine extends GTEngine implements MatchEventListener {
 	}
 
 	/**
-	 * Handles {@link MatchEvent} from Democles.
+	 * Handles the {@link MatchEvent} from Democles.
 	 * 
 	 * @param event
 	 *            the MatchEvent to handle
